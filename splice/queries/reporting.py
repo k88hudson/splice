fad87fad5e4a9836e35d869a53ab1879d18b95ad
k88hudson <k88hudson@gmail.com>
2015-11-12 19:03:29 -0500

    [Fix #138] Added more apis for reporting

--- splice/queries/reporting.py
+++ splice/queries/reporting.py
@@ -1,33 +1,89 @@
 from splice.models import impression_stats_daily
-from splice.queries.tile import get_tiles_by_campaign
+from splice.queries.tile import get_tiles
 from sqlalchemy.sql import func
+from sqlalchemy.orm import aliased
+from sqlalchemy import case
 
 
-def get_stats_by_date(campaign_id):
-
+def get_stats(group_by, filters=None):
     from splice.environment import Environment
     env = Environment.instance()
 
-    tiles = get_tiles_by_campaign(campaign_id)
+    isd = aliased(impression_stats_daily)
+    filters = filters or {}
+    campaign_id = filters.get('campaign_id', None)
+    base_table = isd
+    tiles = []
 
-    if not tiles:
-        return None
+    # Fields
+    if group_by == 'category':
+        grouped_tiles = get_tiles(campaign_id=campaign_id, group_by='category', filters=filters)
+        if not grouped_tiles:
+            return None
+        for c in grouped_tiles:
+            for tile in c['tile_ids']:
+                tiles.append(tile)
 
-    tile_ids = map((lambda tile: tile['id']), tiles)
+        category = case(map(
+            (lambda item: (isd.c.tile_id.in_(item['tile_ids']), item['category'])),
+            grouped_tiles
+        ))
 
+        base_table = (
+            env.db.session.query(
+                category.label('category'),
+                isd.c.date,
+                isd.c.tile_id,
+                isd.c.impressions,
+                isd.c.clicks,
+                isd.c.pinned,
+                isd.c.blocked,
+                isd.c.country_code
+            )
+            .filter(isd.c.tile_id.in_(tiles))
+        ).subquery()
+
+        group_by_column = base_table.c.category
+
+    else:
+        tiles = get_tiles(campaign_id=campaign_id, limit_fields=['id'], filters=filters)
+        if not tiles:
+            return None
+        else:
+            tiles = [t['id'] for t in tiles]
+
+        group_by_column = {
+            'date': isd.c.date,
+            'week': isd.c.week,
+            'month': isd.c.month,
+            'locale': isd.c.locale,
+            'country_code': isd.c.country_code
+        }.get(group_by)
+
+    # Base query
     rows = (
         env.db.session
         .query(
-            impression_stats_daily.c.date,
-            func.sum(impression_stats_daily.c.impressions).label('impressions'),
-            func.sum(impression_stats_daily.c.clicks).label('clicks'),
-            func.sum(impression_stats_daily.c.pinned).label('pinned'),
-            func.sum(impression_stats_daily.c.blocked).label('blocked')
+            group_by_column,
+            func.sum(base_table.c.impressions).label('impressions'),
+            func.sum(base_table.c.clicks).label('clicks'),
+            func.sum(base_table.c.pinned).label('pinned'),
+            func.sum(base_table.c.blocked).label('blocked')
         )
-        .group_by(impression_stats_daily.c.date)
-        .filter(impression_stats_daily.c.tile_id.in_(tile_ids))
-        .order_by(impression_stats_daily.c.date)
-        .all()
+        .group_by(group_by_column)
+        .order_by(group_by_column)
     )
 
-    return [r._asdict() for r in rows]
+    # Filters
+    if not group_by == 'category':
+        rows = rows.filter(base_table.c.tile_id.in_(tiles))
+    if 'country_code' in filters:
+        rows = rows.filter(base_table.c.country_code == filters['country_code'])
+    if 'start_date' in filters:
+        rows = rows.filter(base_table.c.date >= filters['start_date'])
+    if 'end_date' in filters:
+        rows = rows.filter(base_table.c.date <= filters['end_date'])
+
+    rows = rows.all()
+
+    return [r._asdict() for r in rows] if rows else None
--- tests/api/test_reporting.py
+++ tests/api/test_reporting.py
@@ -9,20 +9,51 @@ class TestReportingAPI(BaseTestCase):
     def setUp(self):
         super(TestReportingAPI, self).setUp(load_stats=True)
 
+    def helper_test_set(self, filters={}, status_code=200, base_url='api.reporting.stats', empty=False):
+        group_by = filters.get('group_by')
+        query = {'campaign_id': 1}
+        query.update(filters)
+        url = url_for(base_url, **query)
+        print(url)
+
+        result = self.client.get(url)
+        assert_equal(result.status_code, status_code)
+
+        resp = json.loads(result.data)
+
+        if not empty:
+            assert(len(resp['results']))
+        else:
+            assert_equal(len(resp['results']), 0)
+
+        if group_by:
+            for r in resp['results']:
+                assert(r.get(group_by))
+
+        return resp
+
     def test_reporting_stats(self):
         """Test reporting stats 200 response"""
-        url = url_for('api.reporting.stats', campaign_id=1)
-        res = self.client.get(url)
-        assert_equal(res.status_code, 200)
-        resp = json.loads(res.data)
-        assert(len(resp['results']))
+        default_resp = self.helper_test_set()
+        date_resp = self.helper_test_set(filters={'group_by': 'date'})
+        assert_equal(default_resp, date_resp)
 
-    def test_reporting_stats_404(self):
-        """Test reporting stats 404 response"""
-        url = url_for('api.reporting.stats', campaign_id=1132321)
-        res = self.client.get(url)
-        assert_equal(res.status_code, 404)
+    def test_locale(self):
+        """Test group by locale"""
+        self.helper_test_set(filters={'group_by': 'locale'})
+        self.helper_test_set(filters={'group_by': 'locale', 'country_code': 'US'})
 
-        url = url_for('api.reporting.stats', campaign_id=4)
-        res = self.client.get(url)
-        assert_equal(res.status_code, 404)
+    def test_country(self):
+        """Test group by country_code"""
+        self.helper_test_set(filters={'group_by': 'country_code'})
+        self.helper_test_set(filters={'group_by': 'country_code', 'locale': 'en-US'})
+
+    def test_category(self):
+        """Test group by category"""
+        self.helper_test_set(filters={'group_by': 'category'})
+        self.helper_test_set(filters={'group_by': 'category', 'channel_id': 1})
+
+    def test_reporting_stats_empty(self):
+        """Test reporting stats empty response"""
+        self.helper_test_set({'campaign_id': 1132321}, empty=True)
+        self.helper_test_set({'campaign_id': 4}, empty=True)
